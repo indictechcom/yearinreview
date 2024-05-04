7bb67ea04ab7abb6e74cf17c58466ea2b5450bee
Gopavasanth <gopavasanth1999@gmail.com>
2024-05-04 10:47:14 +0300

    updated dropdowns

--- src/layouts/userInputLayout.vue
+++ src/layouts/userInputLayout.vue
@@ -1,59 +1,98 @@
 <template>
-	<div class="input-layout">
-		<cdx-select
-			required
-			v-model:selected="langselection"
-			:menu-items="languagelist"
-			default-label="Choose a language"
-		/>
-		<cdx-select
-			required
-			v-model:selected="projectselection"
-			:menu-items="wikiprojectlist"
-			default-label="Choose project"
-		/>
-		<cdx-text-input
-			required
-			pattern="^[^:]+$"
-			@keyup.enter="start"
-			type="text"
-			v-model="username"
-			placeholder="What's your username"
-		></cdx-text-input>
-		<cdx-button @click="statclickhandler">Show stats</cdx-button>
-	</div>
+  <div class="input-layout">
+    <cdx-select
+      required
+      :menu-items="languagelist"
+      :selected="selectedLang"
+      @update:selected="handleLanguageChange"
+      default-label="Choose a language"
+    ></cdx-select>
+    <cdx-select
+      required
+      :selected="selectedProject"
+      :menu-items="wikiprojectlist"
+      @update:selected="handleProjectChange"
+      default-label="Choose project"
+    />
+    <cdx-text-input
+      required
+      pattern="^[^:]+$"
+      type="text"
+      v-model="username"
+      placeholder="Whats your username"
+    ></cdx-text-input>
+    <cdx-button @click="statclickhandler">Show stats</cdx-button>
+  </div>
 </template>
 <script>
+import { CdxTextInput, CdxButton, CdxSelect } from "@wikimedia/codex";
 
-import {
-	CdxTextInput,
-	CdxButton,
-	CdxSelect
-} from "@wikimedia/codex";
-
-import { WIKIMEDIA_COMMONS_DROPDOWN_WITH_LANGUAGE } from "../helpers/consts";
+import apiresponse from "../mocks/apiresponse.json";
 
 export default {
-	name: "InputTaker",
-	components: {
-		CdxTextInput,
-		CdxButton,
-		CdxSelect
-	},
-	props: ["statclickhandler"],
-	data() {
-		return {
-			languageCode: "en",
-			project: "wikipedia",
-			username: "",
-			languagelist: [
-				{ label: 'ml', value: 'ml' },
-				{ label: 'en', value: 'en' },
-			],
-			langselection: null,
-			wikiprojectlist: WIKIMEDIA_COMMONS_DROPDOWN_WITH_LANGUAGE,
-			projectselection: null
-		}
-	}
-}
+  name: "InputTaker",
+  components: {
+    CdxTextInput,
+    CdxButton,
+    CdxSelect,
+  },
+  props: ["statclickhandler"],
+  data() {
+    return {
+      project: "wikipedia",
+      selectedProject: null,
+      selectedLang: null,
+      username: "",
+      languagelist: [],
+      wikiprojectlist: [],
+    };
+  },
+  created() {
+    this.updateLanguageList();
+  },
+  methods: {
+    handleLanguageChange(modelValue) {
+      this.selectedLang = modelValue;
+
+      this.updateProjectList();
+    },
+    handleProjectChange(modelValue) {
+      this.selectedProject = modelValue;
+    },
+    async updateLanguageList() {
+      res = await Object.values(apiresponse.sitematrix);
+      if (apiresponse && apiresponse.sitematrix) {
+        this.languagelist = res
+          .map((item) => {
+            return { label: item.name, value: item.code };
+          })
+          .filter(
+            (item) => item.label !== undefined && item.value !== undefined
+          );
+      } else {
+        console.error("Invalid API response format");
+      }
+    },
+    async updateProjectList() {
+      console.log("updateProjectList");
+      console.log(this.selectedLang);
+
+      res = await Object.values(apiresponse.sitematrix);
+      let projectListItem = res.find((item) => item.code === this.selectedLang);
+
+      let projectList = projectListItem.site;
+      if (projectList) {
+        this.wikiprojectlist = projectList
+          .map((item) => {
+            return { label: item.sitename, value: item.code };
+          })
+          .filter(
+            (item) => item.label !== undefined && item.value !== undefined
+          );
+      } else {
+        console.error("Invalid API response format");
+      }
+    },
+  },
+};
 </script>
