020ce80316d3d3f7266dd8575d49f296b6e44cb6
Reputation22 <93476421+varun-s22@users.noreply.github.com>
2024-05-05 12:53:06 +0300

    adds loader and empty state

---
+++ src/components/EmptyState.vue
@@ -0,0 +1,32 @@
+<template>
+    <BaseCard :id="cardId">
+        <template #bCardTop>
+            <h1 class="contribution-header-error">Oops!!</h1>
+            <p class="contribution-subheader">
+                Something went wrong. Please try again later.
+            </p>
+        </template>
+  </BaseCard>
+</template>
+ 
+<script>
+    import BaseCard from "../components/cards/BaseCard";
+    export default {
+        name: 'EmptyState',
+        components: {
+            BaseCard,
+        },
+    }
+</script>
+
+<style>
+    .contribution-header-error {
+        font-weight: 300;
+        margin: 10px auto;
+        color: #cc0000
+    }
+    .contribution-subheader {
+        width: 90%;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/components/FullScreenLoader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="loader-parent">
+        <div class="loader"></div>
+    </div>
+</template>
+ 
+<script>
+    export default {
+        name: 'FullScreenLoader'
+    }
+</script>
+
+<style>
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+    .loader {
+        display:flex;
+        border: 16px solid #f3f3f3;
+        border-top: 16px solid #3498db;
+        border-radius: 50%;
+        width: 80px;
+        height: 80px;
+        animation: spin 2s linear infinite;
+    }
+    .loader-parent {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 110vh;
+        width: 100vw;
+        position: absolute;
+        top: 0;
+        background-color: rgba(0, 0, 0, 0.3);
+        z-index: 10;
+    }
+</style>
